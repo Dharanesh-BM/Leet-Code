007fd0ad621bb2c54a6fdbb9c29a2e08fbb6db5f
Dharanesh-BM <120398347+Dharanesh-BM@users.noreply.github.com>
2024-09-16 18:12:25 +0530

    Time: 20 ms (8.70%) | Memory: 18.8 MB (5.42%) - LeetSync

---
+++ 539-minimum-time-difference/minimum-time-difference.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int convertToMinutes(string time){
+        stringstream ss(time);
+        string temp;
+        int result = 0;
+        for(int i=0;getline(ss,temp,':')&&i<2;i++){
+            if(i==0) result+= stoi(temp)*60;
+            else result += stoi(temp);
+        }
+        
+        return result;
+    }
+
+    int findMinDifference(vector<string>& timePoints) {
+        vector <int> Minutes;
+
+        for(auto i:timePoints){
+            Minutes.push_back(convertToMinutes(i));
+        }
+        // if(find(timePoints.begin(),timePoints.end(),"00:00") != timePoints.end()){
+        //     Minutes.push_back(1440);
+        // }
+
+        sort(Minutes.begin(),Minutes.end());\
+
+        int diff = INT_MAX; 
+    
+        for (int i = 0; i < Minutes.size(); i++){
+            if(i==Minutes.size()-1){
+                if(1440-Minutes[i]+Minutes[0]<=diff){
+                    diff = 1440-Minutes[i]+Minutes[0];
+                }
+            } else {
+                if (Minutes[i + 1] - Minutes[i] < diff) 
+                    diff = Minutes[i + 1] - Minutes[i]; 
+            }
+        }
+        
+        return diff;
+    }
+};
