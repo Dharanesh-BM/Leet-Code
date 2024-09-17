608cb73e720fd0f17b5696631f155b6b97a028e5
Dharanesh-BM <120398347+Dharanesh-BM@users.noreply.github.com>
2024-09-17 06:29:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (45.08%) - LeetSync

--- 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.cpp
+++ 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     vector<string> uncommonFromSentences(string s1, string s2) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        
         unordered_map <string,int> uncommon_words;
         vector <string> result;
         string word;
