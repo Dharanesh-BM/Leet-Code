1df9ca2179cbb52dbef68eb575c337c4c5cfe9a3
Dharanesh-BM <120398347+Dharanesh-BM@users.noreply.github.com>
2024-09-18 22:01:15 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (24.51%) - LeetSync

--- 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.cpp
+++ 1528-kids-with-the-greatest-number-of-candies/kids-with-the-greatest-number-of-candies.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
+
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        
         int max = *max_element(candies.begin(),candies.end());
         vector<bool> result;
         for(auto i:candies){
