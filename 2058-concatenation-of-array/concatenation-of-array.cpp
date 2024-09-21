280fa5f00ed067a23ae94cbd947c9af7e1ed7387
Dharanesh-BM <120398347+Dharanesh-BM@users.noreply.github.com>
2024-09-21 22:44:15 +0530

    Time: 5 ms (57.73%) | Memory: 15.5 MB (36.01%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.cpp
+++ 2058-concatenation-of-array/concatenation-of-array.cpp
@@ -2,9 +2,7 @@ class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
         ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
-        
+
         vector <int> ans = nums;
         ans.insert(ans.end(),nums.begin(),nums.end());
 
