4bf6ec8755b45203606ca180ad793b898cb39e1e
Dharanesh.B.M. <dharanesh2345@gmail.com>
2024-12-14 10:54:04 +0530

    Time: 1166 ms (5.02%) | Memory: 500.7 MB (5.03%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.cpp
+++ 459-repeated-substring-pattern/repeated-substring-pattern.cpp
@@ -7,13 +7,14 @@ public:
         // cout << "first" << s.substr(0,size/2) << endl;
         // cout << "second" << s.substr(size/2,size/2) << endl;
 
+        string repeated = "";
         for(int i = 0; i < size/2; i++){
-            string repeated = "";
             for(int j = 0; j < size/(i+1); j++){
                 repeated.append(s.substr(0,i+1));
             }
             // cout << repeated << endl;
             if(repeated == s) return true;
+            repeated.clear();
         }
         return false;
     }
