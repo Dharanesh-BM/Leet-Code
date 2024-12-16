0008acb90f337b891e6eea880b9bb7267f81b27d
Dharanesh.B.M. <dharanesh2345@gmail.com>
2024-12-16 10:51:31 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (19.35%) - LeetSync

--- 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.cpp
+++ 2238-a-number-after-a-double-reversal/a-number-after-a-double-reversal.cpp
@@ -1,24 +1,7 @@
 class Solution {
 public:
     bool isSameAfterReversals(int num) {
-        int reversed1 = reverseInteger(num);
-        int reversed2 = reverseInteger(reversed1);
-        
-        cout << reversed1 << " " << reversed2 << endl;
-        if(num == reversed2) return true;
-        return false;
-
-    }
-
-    int reverseInteger(int num){
-        int result = 0;
-
-        while(num >= 10){
-            result = result + num%10;
-            result *= 10;
-            num /= 10;
-        }
-
-        return result + num;
+        if(num !=0 && num%10==0) return false;
+        return true;
     }
 };
