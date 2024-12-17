b9aaaebadc13919175af41bb0e79c2de653b16e8
Dharanesh.B.M. <dharanesh2345@gmail.com>
2024-12-17 15:28:53 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (35.14%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
     bool isValid(string s) {
-        vector <char> paranthesis{'.'};
+        stack <char> stack;
         for(auto i:s){
-            if(isOpen(i))
-                paranthesis.push_back(i);
-            else if(isClose(i,paranthesis.back())){
-                paranthesis.pop_back();
-            }
-            else{
-                return false;
+            if(stack.empty()){
+                stack.push(i);
+            } else {
+                if(i==')' && stack.top() == '('){
+                    stack.pop();
+                } else if(i==']' && stack.top() == '['){
+                    stack.pop();
+                } else if(i == '}' && stack.top() == '{'){
+                    stack.pop();
+                } else{
+                    stack.push(i);
+                }
             }
         }
-        return paranthesis.size()==1;
-    }
 
-    bool isOpen(char element){
-        if(element=='(' || element == '{' || element == '[')
-            return true;
-        return false;
-    }
-
-    bool isClose(char a,char b){
-        if(a==')' && b=='(') return true;
-        if(a==']' && b=='[') return true;
-        if(a=='}' && b=='{') return true;
-        return false;
+        return stack.empty();
     }
 };
