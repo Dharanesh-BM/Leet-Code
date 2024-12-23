6feea6a9e9bfc11a865cce160978d6c2316c0542
Dharanesh.B.M. <dharanesh2345@gmail.com>
2024-12-23 12:08:30 +0530

    Time: 6 ms (38.07%) | Memory: 9.8 MB (43.81%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -2,12 +2,9 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
         if(s.length() != t.length()) return false;
-        unordered_map <char,int> s_map,t_map;
-        for(int i = 0; i < s.length(); i++){
-            s_map[s[i]]++;
-            t_map[t[i]]++;
-        }
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
 
-        return (s_map == t_map);
+        return (s == t);
     }
 };
