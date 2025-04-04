70a1419797359ab02ad58e4bcc130c8082ee422c
Dharanesh.B.M. <dharanesh2345@gmail.com>
2025-04-04 06:37:46 +0530

    Time: 489 ms (95.69%) | Memory: 0B (100.00%) - LeetSync

--- 1161-project-employees-i/project-employees-i.sql
+++ 1161-project-employees-i/project-employees-i.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
-select P.project_id, round(avg(cast(E.experience_years as decimal))*1.0,2) as average_years
+select P.project_id, round(avg(E.experience_years),2) as average_years
 from Project P
 join Employee E
 on P.employee_id = E.employee_id
