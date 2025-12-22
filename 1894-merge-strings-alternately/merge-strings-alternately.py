5231d7329ecad797aca340a02d286586d9623f95
Dharanesh.B.M. <dharanesh2345@gmail.com>
2025-12-22 14:21:29 +0530

    Time: 31 ms (90.73%) | Memory: 17.3 MB (98.47%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.py
@@ -0,0 +1,13 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        min_len = min(len(word1),len(word2))
+        ans = ""
+        for i in range(min_len):
+            ans += word1[i] + word2[i]
+        
+        if len(word1) < len(word2):
+            ans += word2[min_len:]
+        else: 
+            ans += word1[min_len:]
+
+        return ans
